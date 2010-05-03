95a32fac768fa4a279e5942ab8e90b4337be7e79
Kim Ahlström <kim@cerego.com>
2010-05-03 23:06:05 +0900

    Broke the MeCab interfacing out into a module and made it work with callbacks

--- example.js
+++ example.js
@@ -1,25 +1,27 @@
+// Example of using the mecab module in a node.js server
+//
+// Parses the path after / with mecab and shows JSON serialized output
+//
+// By Kim Ahlström <kim.ahlstrom@gmail.com> May 2010
+
 var sys = require('sys'), 
     http = require('http'),
-    UTF8 = 'utf8';
-
-var mecabOutput = '';
-var mecabRunning = 0;
-var mecab = process.createChildProcess('mecab');
-mecab.addListener('output', function(data){
-  sys.puts("mecab stdout: " + data);
-  if (data) {
-    mecabOutput = data;
-  }
-});
+    url = require('url'),
+    querystring = require('querystring'),
+		mecab = require('./mecab');
 
 http.createServer(function (req, res) {
-  res.sendHeader(200, {'Content-Type': 'text/plain; charset=UTF8'});
-  m = new process.Promise();
+  res.sendHeader(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  req.setEncoding('utf8');
+  // Need this querystring and url parsing to decode the URL encoding
+  var query = querystring.parse(url.parse(req.url).query)
+  var text = query.text;
+    
+  mecab.parse(text, 'ipadic', function(output){
+    res.write('mecab: '+JSON.stringify(output));  
+    res.end();
+  });
   
-  mecab.write("日本語です\n", UTF8);
-  res.sendBody('mecab: '+JSON.stringify(mecabOutput));  
-  sys.puts("mecab output: "+mecabOutput);
-  res.finish();
 }).listen(8000);
 
 sys.puts('Server running at http://127.0.0.1:8000/');
---
+++ mecab.js
@@ -0,0 +1,89 @@
+// A poor person's interface to MeCab from node.js
+// Parses stdout from mecab instead of using the C interface
+//
+// Exports one function, parse, that does everything you could ever want
+//
+// Tested with node.js version 0.1.91
+//
+// By Kim Ahlström <kim.ahlstrom@gmail.com> May 2010
+
+var spawn = require('child_process').spawn;
+
+var MeCab = (function(){
+  var mecab = spawn('mecab');
+  mecab.stdout.addListener('data', function(data){
+    if (data) {
+      MeCab._wrapper(data);
+    }
+  });
+  
+  return({
+    _mecab: mecab,
+    
+    // The data listener for the mecab child process uses MeCab._wrapper as the callback when it receives data.
+    // So the parse function will attach its own function to this key each time it's called,
+    // so we don't have to attach new listeners each time instead
+    _wrapper: null,
+
+    // Main function, only one exported
+    // text: the text you want to parse
+    // format: the name of the dictionary your mecab is using, must match a key in the formats object
+    // callback: your callback
+    parse: function(text, format, callback){
+      // Attach this function as the actual callback for the data from the mecab child process listener
+      // This is so we can have a new user callback for each time parse is called without having to redefine the mecab listener
+      MeCab._wrapper = function(data){
+        // data is a buffer, so we need to stringify the buffer
+        var string = data.toString('utf8', 0, data.length)
+        
+        // This turns the mecab output into a nice object
+        var parsed = MeCab.formats[format](string);
+        
+        // The user's callback
+        callback(parsed);
+      };
+      
+      // Sets off the parsing
+      MeCab._mecab.stdin.write(text + "\n", 'utf8');
+    },
+    
+    // These functions transform the mecab output into objects that are easy to work with
+    formats: {
+      ipadic: function(data) {
+        var lines = data.split("\n");
+        var result = [];
+        
+        for(line in lines) {
+          if(lines[line] == 'EOS' || lines[line] == '') continue;
+          
+          var parts = lines[line].split(/\s+/);
+          var surface = parts[0];
+          var features = parts[1].split(/,/);
+
+          for(feature in features) {
+            if(features[feature] == '*') {
+              features[feature] = null;
+            }
+          }
+
+          result.push({
+            surface: surface,
+            pos: features[0],
+            pos_detail_1: features[1],
+            pos_detail_2: features[2],
+            pos_detail_3: features[3],
+            inflection: features[4],
+            pattern: features[5],
+            lemma: features[6],
+            reading: features[7],
+            pronunciation: features[8],
+          });
+        }
+        
+        return(result);
+      }
+    }
+  });
+})();
+
+exports.parse = MeCab.parse;
